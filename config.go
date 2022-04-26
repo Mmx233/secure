65f85c9e6b453e65f395c93ea7041062e7a3b733
Mmx233 <admin@mmxblog.com>
2022-04-26 18:53:53 +0800

    feat: 为配置添加默认值

--- config.go
+++ config.go
@@ -27,6 +27,16 @@ func (c *Config) init() {
 			c.AbortWithStatus(403)
 		}
 	}
+	if c.RateLimit == 0 {
+		c.RateLimit = 120
+	}
+	if c.BlackListRate == 0 {
+		c.BlackListRate = 300
+	}
+	if c.BlackListDuration == 0 {
+		c.BlackListDuration = time.Hour / 2
+	}
+
 	c.ipLogger.init()
 	c.normalList.init()
 	c.blackList.init()
